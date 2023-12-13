ef02e00695d182657a0a867b542b3b3f483523f2
Eric Griffin <ejgriffin13@gmail.com>
2023-12-12 20:09:18 -0500

    Repaired PostgreSQL database schema.

--- api/users.js
+++ api/users.js
@@ -1,34 +1,31 @@
-const express = require('express');
+const express = require("express");
 const usersRouter = express.Router();
 
-const { 
-  createUser,
-  getAllUsers,
-  getUserByUsername,
-} = require('../db');
+const { createUser, getAllUsers, getUserByUsername } = require("../db");
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-usersRouter.get('/', async (req, res, next) => {
+usersRouter.get("/", async (req, res, next) => {
   try {
     const users = await getAllUsers();
-  
+
     res.send({
-      users
+      users,
     });
   } catch ({ name, message }) {
     next({ name, message });
   }
 });
 
-usersRouter.post('/login', async (req, res, next) => {
+// /api/users/login
+usersRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
 
   // request must have both
   if (!username || !password) {
     next({
       name: "MissingCredentialsError",
-      message: "Please supply both a username and password"
+      message: "Please supply both a username and password",
     });
   }
 
@@ -36,39 +33,43 @@ usersRouter.post('/login', async (req, res, next) => {
     const user = await getUserByUsername(username);
 
     if (user && user.password == password) {
-      const token = jwt.sign({ 
-        id: user.id, 
-        username
-      }, process.env.JWT_SECRET, {
-        expiresIn: '1w'
-      });
+      const token = jwt.sign(
+        {
+          id: user.id,
+          username,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1w",
+        }
+      );
 
-      res.send({ 
+      res.send({
         message: "you're logged in!",
-        token 
+        token,
       });
     } else {
-      next({ 
-        name: 'IncorrectCredentialsError', 
-        message: 'Username or password is incorrect'
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
       });
     }
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     next(error);
   }
 });
 
-usersRouter.post('/register', async (req, res, next) => {
+usersRouter.post("/register", async (req, res, next) => {
   const { username, password, name, location } = req.body;
 
   try {
     const _user = await getUserByUsername(username);
-  
+
     if (_user) {
       next({
-        name: 'UserExistsError',
-        message: 'A user by that username already exists'
+        name: "UserExistsError",
+        message: "A user by that username already exists",
       });
     }
 
@@ -79,20 +80,24 @@ usersRouter.post('/register', async (req, res, next) => {
       location,
     });
 
-    const token = jwt.sign({ 
-      id: user.id, 
-      username
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1w'
-    });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
 
-    res.send({ 
+    res.send({
       message: "thank you for signing up",
-      token 
+      token,
     });
   } catch ({ name, message }) {
     next({ name, message });
-  } 
+  }
 });
 
 module.exports = usersRouter;
